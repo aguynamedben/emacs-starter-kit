409f2855e09c0bed1d052b2a44501bbc0b420301
Ben Standefer <benstandefer@gmail.com>
2010-09-01 14:59:19 -0700

    Add comment for dirtrack-list to work

--- custom.el
+++ custom.el
@@ -3,7 +3,10 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+
+ ;; For this to work, your prompt needs to be set to export PS1='\h!\u:\w\$ '
  '(dirtrack-list (quote ("\\((.+)\\)?[a-z0-9]+.[a-z0-9]+:\\(.*\\)[$#] $" 2)))
+ 
  '(explicit-bash-args (quote ("--noediting" "-i" "-l")))
  '(ido-mode nil nil (ido))
  '(iswitchb-mode t)
