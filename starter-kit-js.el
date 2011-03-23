3b05a103aa406774d76deb009bab294452230c3d
Ben Standefer <benstandefer@gmail.com>
2011-03-22 17:12:45 -0700

    Added CoffeeScript configs.

--- starter-kit-js.el
+++ starter-kit-js.el
@@ -25,4 +25,10 @@
                                        nil)))))))
 
 (provide 'starter-kit-js)
+(require 'coffee-mode)
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (set (make-local-variable 'tab-width) 2))
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
 ;;; starter-kit-js.el ends here
