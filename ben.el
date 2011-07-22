f07ad08ff52c369060414df0adbf4b33d75ff033
Ben Standefer <benstandefer@gmail.com>
2011-07-22 01:58:58 -0700

    Add SSH convenience functions to make connection to preset servers easier.

--- ben.el
+++ ben.el
@@ -99,3 +99,35 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  )
+
+
+;; Make connecting to servers easier
+(defvar ssh-connection-alist
+  '())
+
+(setq ssh-connection-alist
+  '((sgdev . "ec2-204-236-167-168.us-west-1.compute.amazonaws.com")
+    (sgtail . "ec2-204-236-167-168.us-west-1.compute.amazonaws.com")
+    (sggate . "ec2-204-236-167-168.us-west-1.compute.amazonaws.com")
+    (sgpushpin . "ec2-204-236-167-168.us-west-1.compute.amazonaws.com")
+    (sgbulkloader . "ec2-204-236-167-168.us-west-1.compute.amazonaws.com")
+
+    (bendev . "ec2-184-72-11-102.us-west-1.compute.amazonaws.com")))
+
+(defun ssh-convenience ()
+  "Make SSH convenience functions."
+  (interactive)
+  (mapcar (lambda (conn)
+            (let ((name (symbol-name (car conn)))
+                  (host (cdr conn)))
+              (fset (intern name)
+                    `(lambda nil
+                       ,(format "Connect to %s SSH preset." name)
+                       (interactive)
+                       (let ((buffer-name ,(format "*ssh-%s*" name)))
+                         (if (get-buffer buffer-name)
+                             (pop-to-buffer buffer-name)
+                           (ssh ,host (pop-to-buffer buffer-name))))))))
+          ssh-connection-alist))
+
+(ssh-convenience)
